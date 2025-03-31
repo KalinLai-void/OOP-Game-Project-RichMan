fc237425f9f421d5b5a4e2d919b0531fb27c4f29
KuanI <carterwu1120@gmail.com>
2025-03-31 23:30:11 +0800

    chore: remove all feature

--- monopoly/Main.cpp
+++ monopoly/Main.cpp
@@ -5,14 +5,18 @@
 using namespace std;
 int main() {
     GameMode mode = GameMode::DUEL;
+    /*
     GameConfig::getInstance().setMode(mode);
+    */
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     // Game game(GameConfig::getInstance());
+    /*
     std::shared_ptr<Game> game = Game::getInstance(GameConfig::getInstance());
     game->initGame();
     game->start();
+    */
 
     // End of the game
     SingletonManager::destroyAll(); // Destroy all singleton instances
